2314ae85696003f5a8283586246b792f92043fb0
Josh <joshuabigbadboy@hotmail.co.uk>
2019-02-17 20:26:46 +0000

    A clean 1.13.2 slate.

--- java/com/joshmanisdabomb/aimagg/items/AimaggItemColored.java
+++
@@ -1,9 +0,0 @@
-package com.joshmanisdabomb.aimagg.items;
-
-import net.minecraft.item.ItemStack;
-
-public interface AimaggItemColored {
-
-	public int getColorFromItemstack(ItemStack stack, int tintIndex);
-	
-}
--- java/com/joshmanisdabomb/aimagg/items/AimaggItemGauntlet.java
+++
@@ -1,9 +0,0 @@
-package com.joshmanisdabomb.aimagg.items;
-
-public class AimaggItemGauntlet extends AimaggItemBasic {
-
-	public AimaggItemGauntlet(String internalName) {
-		super(internalName);
-	}
-
-}

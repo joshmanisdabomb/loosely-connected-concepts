49aad0aaf22da517bb18a8af660318f592dd1169
Josh <joshmanisdabomb@gmail.com>
2019-07-20 21:48:29 +0100

    Adding survival-based block properties.

--- java/com/joshmanisdabomb/lcc/registry/LCCBlocks.java
+++ java/com/joshmanisdabomb/lcc/registry/LCCBlocks.java
@@ -9,6 +9,7 @@ import net.minecraft.block.material.MaterialColor;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.DyeColor;
 import net.minecraft.item.Item;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.event.RegistryEvent.Register;
 
 import java.util.ArrayList;
@@ -44,43 +45,43 @@ public abstract class LCCBlocks {
 
 	public static void init(Register<Block> e) {
 		//Test Blocks
-		all.add(test_block = new Block(Block.Properties.create(Material.ROCK, DyeColor.YELLOW)).setRegistryName(LCC.MODID, "test_block"));
+		all.add(test_block = new Block(Block.Properties.create(Material.EARTH, DyeColor.YELLOW).hardnessAndResistance(0.5F).sound(SoundType.SCAFFOLDING)).setRegistryName(LCC.MODID, "test_block"));
 		createDefaultBlockItem(test_block);
-		all.add(test_block_2 = new TestHorizontalBlock(Block.Properties.create(Material.ROCK, DyeColor.YELLOW)).setRegistryName(LCC.MODID, "test_block_2"));
+		all.add(test_block_2 = new TestHorizontalBlock(Block.Properties.create(Material.EARTH, DyeColor.YELLOW).hardnessAndResistance(0.5F).sound(SoundType.SCAFFOLDING)).setRegistryName(LCC.MODID, "test_block_2"));
 		createDefaultBlockItem(test_block_2);
-		all.add(test_block_3 = new TestDirectionalBlock(Block.Properties.create(Material.ROCK, DyeColor.YELLOW)).setRegistryName(LCC.MODID, "test_block_3"));
+		all.add(test_block_3 = new TestDirectionalBlock(Block.Properties.create(Material.EARTH, DyeColor.YELLOW).hardnessAndResistance(0.5F).sound(SoundType.SCAFFOLDING)).setRegistryName(LCC.MODID, "test_block_3"));
 		createDefaultBlockItem(test_block_3);
-		all.add(test_block_4 = new TestPillarBlock(Block.Properties.create(Material.ROCK, DyeColor.YELLOW)).setRegistryName(LCC.MODID, "test_block_4"));
+		all.add(test_block_4 = new TestPillarBlock(Block.Properties.create(Material.EARTH, DyeColor.YELLOW).hardnessAndResistance(0.5F).sound(SoundType.SCAFFOLDING)).setRegistryName(LCC.MODID, "test_block_4"));
 		createDefaultBlockItem(test_block_4);
-		all.add(test_block_5 = new Block(Block.Properties.create(Material.ROCK, DyeColor.YELLOW)).setRegistryName(LCC.MODID, "test_block_5"));
+		all.add(test_block_5 = new Block(Block.Properties.create(Material.EARTH, DyeColor.YELLOW).hardnessAndResistance(0.5F).sound(SoundType.SCAFFOLDING)).setRegistryName(LCC.MODID, "test_block_5"));
 		createDefaultBlockItem(test_block_5);
 
 		//Resources
-		all.add(ruby_ore = new Block(Block.Properties.create(Material.ROCK)).setRegistryName(LCC.MODID, "ruby_ore"));
+		all.add(ruby_ore = new Block(Block.Properties.create(Material.ROCK).harvestTool(ToolType.PICKAXE).harvestLevel(2).hardnessAndResistance(3.0F)).setRegistryName(LCC.MODID, "ruby_ore"));
 		createDefaultBlockItem(ruby_ore);
-		all.add(ruby_storage = new Block(Block.Properties.create(Material.ROCK, MaterialColor.TNT)).setRegistryName(LCC.MODID, "ruby_storage"));
+		all.add(ruby_storage = new Block(Block.Properties.create(Material.ROCK, MaterialColor.TNT).harvestTool(ToolType.PICKAXE).harvestLevel(2).hardnessAndResistance(5.0F, 6.0F)).setRegistryName(LCC.MODID, "ruby_storage"));
 		createDefaultBlockItem(ruby_storage);
-		all.add(topaz_ore = new Block(Block.Properties.create(Material.ROCK)).setRegistryName(LCC.MODID, "topaz_ore"));
+		all.add(topaz_ore = new Block(Block.Properties.create(Material.ROCK).harvestTool(ToolType.PICKAXE).harvestLevel(2).hardnessAndResistance(3.0F)).setRegistryName(LCC.MODID, "topaz_ore"));
 		createDefaultBlockItem(topaz_ore);
-		all.add(topaz_storage = new Block(Block.Properties.create(Material.ROCK, MaterialColor.WHITE_TERRACOTTA)).setRegistryName(LCC.MODID, "topaz_storage"));
+		all.add(topaz_storage = new Block(Block.Properties.create(Material.ROCK, MaterialColor.WHITE_TERRACOTTA).harvestTool(ToolType.PICKAXE).harvestLevel(2).hardnessAndResistance(5.0F, 6.0F)).setRegistryName(LCC.MODID, "topaz_storage"));
 		createDefaultBlockItem(topaz_storage);
-		all.add(sapphire_ore = new Block(Block.Properties.create(Material.ROCK)).setRegistryName(LCC.MODID, "sapphire_ore"));
+		all.add(sapphire_ore = new Block(Block.Properties.create(Material.ROCK).harvestTool(ToolType.PICKAXE).harvestLevel(2).hardnessAndResistance(3.0F)).setRegistryName(LCC.MODID, "sapphire_ore"));
 		createDefaultBlockItem(sapphire_ore);
-		all.add(sapphire_storage = new Block(Block.Properties.create(Material.ROCK, DyeColor.BLUE)).setRegistryName(LCC.MODID, "sapphire_storage"));
+		all.add(sapphire_storage = new Block(Block.Properties.create(Material.ROCK, DyeColor.BLUE).harvestTool(ToolType.PICKAXE).harvestLevel(2).hardnessAndResistance(5.0F, 6.0F)).setRegistryName(LCC.MODID, "sapphire_storage"));
 		createDefaultBlockItem(sapphire_storage);
-		all.add(amethyst_ore = new Block(Block.Properties.create(Material.ROCK)).setRegistryName(LCC.MODID, "amethyst_ore"));
+		all.add(amethyst_ore = new Block(Block.Properties.create(Material.ROCK).harvestTool(ToolType.PICKAXE).harvestLevel(2).hardnessAndResistance(3.0F)).setRegistryName(LCC.MODID, "amethyst_ore"));
 		createDefaultBlockItem(amethyst_ore);
-		all.add(amethyst_storage = new Block(Block.Properties.create(Material.ROCK, DyeColor.PURPLE)).setRegistryName(LCC.MODID, "amethyst_storage"));
+		all.add(amethyst_storage = new Block(Block.Properties.create(Material.ROCK, DyeColor.PURPLE).harvestTool(ToolType.PICKAXE).harvestLevel(2).hardnessAndResistance(5.0F, 6.0F)).setRegistryName(LCC.MODID, "amethyst_storage"));
 		createDefaultBlockItem(amethyst_storage);
-		all.add(uranium_ore = new Block(Block.Properties.create(Material.ROCK)).setRegistryName(LCC.MODID, "uranium_ore"));
+		all.add(uranium_ore = new Block(Block.Properties.create(Material.ROCK).harvestTool(ToolType.PICKAXE).harvestLevel(3).hardnessAndResistance(3.0F)).setRegistryName(LCC.MODID, "uranium_ore"));
 		createDefaultBlockItem(uranium_ore);
-		all.add(uranium_storage = new Block(Block.Properties.create(Material.ROCK, DyeColor.LIME)).setRegistryName(LCC.MODID, "uranium_storage"));
+		all.add(uranium_storage = new Block(Block.Properties.create(Material.ROCK, DyeColor.LIME).harvestTool(ToolType.PICKAXE).harvestLevel(3).hardnessAndResistance(5.0F, 6.0F)).setRegistryName(LCC.MODID, "uranium_storage"));
 		createDefaultBlockItem(uranium_storage);
-		all.add(enriched_uranium_storage = new Block(Block.Properties.create(Material.ROCK, DyeColor.LIME)).setRegistryName(LCC.MODID, "enriched_uranium_storage"));
+		all.add(enriched_uranium_storage = new Block(Block.Properties.create(Material.ROCK, DyeColor.LIME).harvestTool(ToolType.PICKAXE).harvestLevel(3).hardnessAndResistance(5.0F, 6.0F)).setRegistryName(LCC.MODID, "enriched_uranium_storage"));
 		createDefaultBlockItem(enriched_uranium_storage);
 
 		//Nuclear
-		all.add(nuclear_waste = new NuclearWasteBlock(Block.Properties.create(Material.ROCK, MaterialColor.CYAN_TERRACOTTA).hardnessAndResistance(-1.0F, 3600000.0F)).setRegistryName(LCC.MODID, "nuclear_waste"));
+		all.add(nuclear_waste = new NuclearWasteBlock(Block.Properties.create(Material.ROCK, MaterialColor.CYAN_TERRACOTTA).hardnessAndResistance(-1.0F, 3600000.0F).sound(SoundType.CORAL)).setRegistryName(LCC.MODID, "nuclear_waste"));
 		createDefaultBlockItem(nuclear_waste);
 		all.add(nuclear_fire = new NuclearFireBlock(Block.Properties.create(Material.FIRE, DyeColor.LIME).doesNotBlockMovement().tickRandomly().hardnessAndResistance(0.0F).lightValue(15).sound(SoundType.CLOTH)).setRegistryName(LCC.MODID, "nuclear_fire"));
 	}

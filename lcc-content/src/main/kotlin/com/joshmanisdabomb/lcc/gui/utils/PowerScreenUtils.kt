51b8e81ff39ff1b9a6108cb082e1d189bda1337f
Josh <joshmanisdabomb@gmail.com>
2021-01-25 18:02:43 +0000

    Fixed power screens crashing in live environment. Fixed fabric loader requirement in JSON.

--- lcc-content/src/main/kotlin/com/joshmanisdabomb/lcc/gui/utils/PowerScreenUtils.kt
+++ lcc-content/src/main/kotlin/com/joshmanisdabomb/lcc/gui/utils/PowerScreenUtils.kt
@@ -3,6 +3,7 @@ package com.joshmanisdabomb.lcc.gui.utils
 import com.joshmanisdabomb.lcc.energy.LooseEnergy
 import com.joshmanisdabomb.lcc.extensions.decimalFormat
 import net.minecraft.client.font.TextRenderer
+import net.minecraft.client.gui.screen.Screen
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.text.OrderedText
 import net.minecraft.text.TranslatableText
@@ -25,69 +26,70 @@ interface PowerScreenUtils {
 
     fun renderPower(matrices: MatrixStack, power: Float, maxPower: Float, x: Int, y: Int) {
         val h = ceil(13.times(power.div(maxPower))).toInt()
-        drawTexture(matrices, x, y + 13.minus(h), offsetX, power_v + 13.minus(h), 11, h)
+        draw(matrices, x, y + 13.minus(h), offsetX, power_v + 13.minus(h), 11, h)
     }
 
     fun renderBurn(matrices: MatrixStack, power: Int, maxBurn: Int, x: Int, y: Int) {
         val h = ceil(13.times(power.toFloat().div(maxBurn))).toInt()
-        drawTexture(matrices, x, y + 13.minus(h), offsetX, burn_v + 13.minus(h), 14, h)
+        draw(matrices, x, y + 13.minus(h), offsetX, burn_v + 13.minus(h), 14, h)
     }
 
     fun renderProgress(matrices: MatrixStack, progress: Int, maxProgress: Int, x: Int, y: Int) {
         val w = ceil(23.times(progress.toFloat().div(maxProgress))).toInt()
-        this.drawTexture(matrices, x, y, offsetX.plus(1), progress_v, w, 17)
+        this.draw(matrices, x, y, offsetX.plus(1), progress_v, w, 17)
     }
 
     fun renderAction(matrices: MatrixStack, icon: Int, efficiency: Float, maxEfficiency: Float, x: Int, y: Int) {
-        this.drawTexture(matrices, x, y, offsetX, action_v + icon.times(14), 14, 14)
+        this.draw(matrices, x, y, offsetX, action_v + icon.times(14), 14, 14)
         val eff = efficiency.div(maxEfficiency)
         if (eff >= 1f && ticks % 14 >= 7) {
-            this.drawTexture(matrices, x, y, offsetX.plus(28), action_v + icon.times(14), 14, 14)
+            this.draw(matrices, x, y, offsetX.plus(28), action_v + icon.times(14), 14, 14)
         } else {
             val h2 = ceil(13.times(eff)).toInt()
-            this.drawTexture(matrices, x, y + 13.minus(h2), offsetX.plus(14), action_v + icon.times(14) + 13.minus(h2), 14, h2)
+            this.draw(matrices, x, y + 13.minus(h2), offsetX.plus(14), action_v + icon.times(14) + 13.minus(h2), 14, h2)
         }
     }
 
     fun renderPowerBar(matrices: MatrixStack, power: Float, maxPower: Float, x: Int, y: Int, w: Int) {
         val w2 = ceil(w.times(power.div(maxPower))).toInt()
-        drawTexture(matrices, x, y, 0, offsetY, w2, 10)
+        draw(matrices, x, y, 0, offsetY, w2, 10)
     }
 
     fun renderPowerTooltip(matrices: MatrixStack, power: Float, consumed: Float?, mouseX: Int, mouseY: Int, x: IntRange, y: IntRange) {
         val prefix = if (consumed != null) ".recipe" else ""
         if (mouseX in x && mouseY in y) {
-            renderOrderedTooltip(matrices, textRenderer.wrapLines(TranslatableText("$translationKey.power".plus(prefix), LooseEnergy.displayWithUnits(LooseEnergy.fromStandard(power)), LooseEnergy.displayWithUnits(consumed ?: 0f)), Int.MAX_VALUE), mouseX, mouseY)
+            tooltip(matrices, textRenderer.wrapLines(TranslatableText("$translationKey.power".plus(prefix), LooseEnergy.displayWithUnits(LooseEnergy.fromStandard(power)), LooseEnergy.displayWithUnits(consumed ?: 0f)), Int.MAX_VALUE), mouseX, mouseY)
         }
     }
 
     fun renderProgressTooltip(matrices: MatrixStack, progress: Int, maxProgress: Int, mouseX: Int, mouseY: Int, x: IntRange, y: IntRange) {
         if (mouseX in x && mouseY in y) {
-            renderOrderedTooltip(matrices, textRenderer.wrapLines(TranslatableText("$translationKey.time", ceil(progress.div(20f)).toInt(), ceil(maxProgress.div(20f)).toInt()), Int.MAX_VALUE), mouseX, mouseY)
+            tooltip(matrices, textRenderer.wrapLines(TranslatableText("$translationKey.time", ceil(progress.div(20f)).toInt(), ceil(maxProgress.div(20f)).toInt()), Int.MAX_VALUE), mouseX, mouseY)
         }
     }
 
     fun renderActionTooltip(matrices: MatrixStack, efficiency: Float, maxEfficiency: Float?, mouseX: Int, mouseY: Int, x: IntRange, y: IntRange) {
         val prefix = if (maxEfficiency != null) ".recipe" else ""
         if (mouseX in x && mouseY in y) {
-            renderOrderedTooltip(matrices, textRenderer.wrapLines(TranslatableText("$translationKey.efficiency".plus(prefix), efficiency.plus(100).decimalFormat(force = true), (maxEfficiency ?: 0f).plus(100).decimalFormat(force = true)), Int.MAX_VALUE), mouseX, mouseY)
+            tooltip(matrices, textRenderer.wrapLines(TranslatableText("$translationKey.efficiency".plus(prefix), efficiency.plus(100).decimalFormat(force = true), (maxEfficiency ?: 0f).plus(100).decimalFormat(force = true)), Int.MAX_VALUE), mouseX, mouseY)
         }
     }
 
     fun renderBurnTooltip(matrices: MatrixStack, progress: Int, maxProgress: Int, mouseX: Int, mouseY: Int, x: IntRange, y: IntRange) {
         if (mouseX in x && mouseY in y) {
-            renderOrderedTooltip(matrices, textRenderer.wrapLines(TranslatableText("$translationKey.burn", ceil(progress.div(20f)).toInt(), ceil(maxProgress.div(20f)).toInt()), Int.MAX_VALUE), mouseX, mouseY)
+            tooltip(matrices, textRenderer.wrapLines(TranslatableText("$translationKey.burn", ceil(progress.div(20f)).toInt(), ceil(maxProgress.div(20f)).toInt()), Int.MAX_VALUE), mouseX, mouseY)
         }
     }
 
     fun renderSteamTooltip(matrices: MatrixStack, output: Float, outputCeil: Float, water: Int, mouseX: Int, mouseY: Int, x: IntRange, y: IntRange) {
         if (mouseX in x && mouseY in y) {
-            renderOrderedTooltip(matrices, textRenderer.wrapLines(TranslatableText("$translationKey.output", LooseEnergy.displayWithUnits(output), LooseEnergy.displayWithUnits(outputCeil), water.div(3f).decimalFormat(force = true)), Int.MAX_VALUE), mouseX, mouseY)
+            tooltip(matrices, textRenderer.wrapLines(TranslatableText("$translationKey.output", LooseEnergy.displayWithUnits(output), LooseEnergy.displayWithUnits(outputCeil), water.div(3f).decimalFormat(force = true)), Int.MAX_VALUE), mouseX, mouseY)
         }
     }
 
-    fun drawTexture(matrices: MatrixStack, x: Int, y: Int, u: Int, v: Int, width: Int, height: Int)
+    @JvmDefault
+    private fun draw(matrices: MatrixStack, x: Int, y: Int, u: Int, v: Int, width: Int, height: Int) = (this as Screen).drawTexture(matrices, x, y, u, v, width, height)
 
-    fun renderOrderedTooltip(matrices: MatrixStack, wrapLines: List<OrderedText>, mouseX: Int, mouseY: Int)
+    private fun tooltip(matrices: MatrixStack, wrapLines: List<OrderedText>, mouseX: Int, mouseY: Int) = (this as Screen).renderOrderedTooltip(matrices, wrapLines, mouseX, mouseY)
 
 }

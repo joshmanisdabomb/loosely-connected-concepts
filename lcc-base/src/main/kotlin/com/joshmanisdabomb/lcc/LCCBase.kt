4e843946fb53ef14e64870c91453d3d14e4941a1
Josh <joshmanisdabomb@gmail.com>
2021-02-19 01:07:25 +0000

    Fixed server crash on trying to initialise base client packets.

--- lcc-base/src/main/kotlin/com/joshmanisdabomb/lcc/LCCBase.kt
+++ lcc-base/src/main/kotlin/com/joshmanisdabomb/lcc/LCCBase.kt
@@ -1,12 +1,11 @@
 package com.joshmanisdabomb.lcc
 
-import com.joshmanisdabomb.lcc.directory.LCCBasePacketsToClient
 import net.fabricmc.api.ModInitializer
 
 object LCCBase : ModInitializer {
 
     override fun onInitialize() {
-        LCCBasePacketsToClient.init()
+
     }
 
 }

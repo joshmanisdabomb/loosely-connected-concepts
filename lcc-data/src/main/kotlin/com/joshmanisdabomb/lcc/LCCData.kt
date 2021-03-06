272d0c576a4a8db83f35ddba6dd3c9d1830ba57c
Josh <joshmanisdabomb@gmail.com>
2021-03-06 15:14:30 +0000

    Added sound data generator.

--- lcc-data/src/main/kotlin/com/joshmanisdabomb/lcc/LCCData.kt
+++ lcc-data/src/main/kotlin/com/joshmanisdabomb/lcc/LCCData.kt
@@ -4,6 +4,7 @@ import com.joshmanisdabomb.lcc.data.DataAccessor
 import com.joshmanisdabomb.lcc.data.LCCLangData
 import com.joshmanisdabomb.lcc.data.directory.*
 import com.joshmanisdabomb.lcc.data.generators.CommitData
+import com.joshmanisdabomb.lcc.data.generators.SoundData
 import me.shedaniel.cloth.api.datagen.v1.DataGeneratorHandler
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint
 import java.nio.file.Paths
@@ -13,6 +14,8 @@ object LCCData : PreLaunchEntrypoint {
 
     val path = Paths.get("../lcc-content/src/generated/resources")
     val accessor = DataAccessor("lcc", DataGeneratorHandler.create(path), listOf("en_us", "en_gb"))
+
+    val sounds = SoundData(accessor.handler.dataGenerator, "lcc")
 
     override fun onPreLaunch() {
         LCC.onInitialize()
@@ -27,6 +30,9 @@ object LCCData : PreLaunchEntrypoint {
 
         LCCAdvancementData.init()
         LCCLangData(accessor.lang)
+
+        LCCSoundData.init()
+        accessor.handler.install(sounds)
 
         accessor.handler.install(CommitData(path, Paths.get("../lcc-content/src/main/resources")) { CommitData.defaultExcluder(it, LCC.modid, "fabric", "minecraft") })
 
---
+++ lcc-data/src/main/kotlin/com/joshmanisdabomb/lcc/data/directory/LCCSoundData.kt
@@ -0,0 +1,72 @@
+package com.joshmanisdabomb.lcc.data.directory
+
+import com.joshmanisdabomb.lcc.LCC
+import com.joshmanisdabomb.lcc.LCCData
+import com.joshmanisdabomb.lcc.data.json.sound.SoundProperties
+import com.joshmanisdabomb.lcc.directory.BasicDirectory
+import com.joshmanisdabomb.lcc.directory.LCCSounds
+
+object LCCSoundData: BasicDirectory<SoundProperties, Unit>() {
+
+    val soaking_soul_sand_break by entry(::initialiser) { SoundProperties("block", LCC.modid, "soaking_soul_sand.break", Array(4) {
+        SoundProperties.SoundEntry("${LCC.modid}:dig/soaking_soul_sand${it+1}", volume = 0.8f)
+    }, "subtitles.block.generic.break") }
+    val soaking_soul_sand_fall by entry(::initialiser) { SoundProperties("block", LCC.modid, "soaking_soul_sand.fall", Array(5) {
+        SoundProperties.SoundEntry("${LCC.modid}:step/soaking_soul_sand${it+1}")
+    }, null) }
+    val soaking_soul_sand_hit by entry(::initialiser) { SoundProperties("block", LCC.modid, "soaking_soul_sand.hit", Array(5) {
+        SoundProperties.SoundEntry("${LCC.modid}:step/soaking_soul_sand${it+1}")
+    }, "subtitles.block.generic.hit") }
+    val soaking_soul_sand_place by entry(::initialiser) { SoundProperties("block", LCC.modid, "soaking_soul_sand.place", Array(4) {
+        SoundProperties.SoundEntry("${LCC.modid}:dig/soaking_soul_sand${it+1}", volume = 0.8f)
+    }, "subtitles.block.generic.place") }
+    val soaking_soul_sand_step by entry(::initialiser) { SoundProperties("block", LCC.modid, "soaking_soul_sand.step", Array(5) {
+        SoundProperties.SoundEntry("${LCC.modid}:step/soaking_soul_sand${it+1}")
+    }, "subtitles.block.generic.footsteps") }
+    val soaking_soul_sand_jump by entry(::initialiser) { SoundProperties("block", LCC.modid, "soaking_soul_sand.jump", Array(1) {
+        SoundProperties.SoundEntry("${LCC.modid}:block/soaking_soul_sand_jump")
+    }) }
+
+    val bounce_pad_jump by entry(::initialiser) { SoundProperties("block", LCC.modid, "bounce_pad.jump", Array(1) {
+        SoundProperties.SoundEntry("${LCC.modid}:block/bounce_pad_jump")
+    }) }
+    val bounce_pad_set by entry(::initialiser) { SoundProperties("block", LCC.modid, "bounce_pad.set", Array(1) {
+        SoundProperties.SoundEntry("minecraft:random/click")
+    }) }
+
+    val pocket_zombie_pigman_ambient by entry(::initialiser) { SoundProperties("entity", LCC.modid, "pocket_zombie_pigman.ambient", Array(4) {
+        SoundProperties.SoundEntry("minecraft:mob/zombified_piglin/zpigangry${it+1}")
+    }) }
+    val pocket_zombie_pigman_hurt by entry(::initialiser) { SoundProperties("entity", LCC.modid, "pocket_zombie_pigman.hurt", Array(2) {
+        SoundProperties.SoundEntry("minecraft:mob/zombified_piglin/zpighurt${it+1}")
+    }) }
+    val pocket_zombie_pigman_death by entry(::initialiser) { SoundProperties("entity", LCC.modid, "pocket_zombie_pigman.death", Array(1) {
+        SoundProperties.SoundEntry("minecraft:mob/zombified_piglin/zpigdeath")
+    }) }
+
+    val classic_crying_obsidian_set_spawn by entry(::initialiser) { SoundProperties("block", LCC.modid, "classic_crying_obsidian.set_spawn", Array(3) {
+        SoundProperties.SoundEntry("minecraft:block/respawn_anchor/set_spawn${it+1}") //TODO custom
+    }) }
+
+    val atomic_bomb_fuse by entry(::initialiser) { SoundProperties("entity", LCC.modid, "atomic_bomb.fuse", Array(1) {
+        SoundProperties.SoundEntry("${LCC.modid}:entity/atomicbomb/fuse")
+    }) }
+    val atomic_bomb_defuse by entry(::initialiser) { SoundProperties("entity", LCC.modid, "atomic_bomb.defuse", Array(5) {
+        SoundProperties.SoundEntry("${LCC.modid}:entity/atomicbomb/cut${it+1}")
+    }) }
+    val nuclear_explosion_explode by entry(::initialiser) { SoundProperties("entity", LCC.modid, "nuclear_explosion.explode", Array(1) {
+        SoundProperties.SoundEntry("${LCC.modid}:entity/nuclearexplosion/explode", stream = true)
+    }) }
+
+    fun initialiser(input: SoundProperties, context: DirectoryContext<Unit>, parameters: Unit) = input.also { LCCData.sounds.sounds.add(it) }
+
+    override fun afterInitAll(initialised: List<DirectoryEntry<out SoundProperties, out SoundProperties>>, filter: (context: DirectoryContext<Unit>) -> Boolean) {
+        val a = LCCSoundData.all.keys.minus(LCCSounds.all.keys)
+        if (a.isNotEmpty()) println("LCCSoundData contains information not present in LCCSounds: $a")
+        val b = LCCSounds.all.keys.minus(LCCSoundData.all.keys)
+        if (b.isNotEmpty()) println("LCCSounds contains defined sounds not present in LCCSoundData. $b")
+    }
+
+    override fun defaultProperties(name: String) = Unit
+
+}

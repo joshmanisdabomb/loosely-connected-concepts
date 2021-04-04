47cf2441b4648932650ed8d75c6cc99180aa2545
Josh <joshmanisdabomb@gmail.com>
2021-04-04 19:37:04 +0100

    Nuclear waste now falls in a pile effect and can slide off of other nuclear waste blocks.

--- lcc-content/src/main/kotlin/com/joshmanisdabomb/lcc/block/NuclearWasteBlock.kt
+++ lcc-content/src/main/kotlin/com/joshmanisdabomb/lcc/block/NuclearWasteBlock.kt
@@ -6,6 +6,7 @@ import com.joshmanisdabomb.lcc.adaptation.LCCExtendedBlock
 import com.joshmanisdabomb.lcc.adaptation.LCCExtendedBlockContent
 import com.joshmanisdabomb.lcc.directory.LCCBlocks
 import com.joshmanisdabomb.lcc.directory.LCCDamage
+import com.joshmanisdabomb.lcc.extensions.horizontalDirections
 import com.joshmanisdabomb.lcc.extensions.transformInt
 import com.joshmanisdabomb.lcc.item.HazmatArmorItem
 import com.joshmanisdabomb.lcc.item.HazmatTankArmorItem
@@ -26,7 +27,7 @@ import java.util.*
 class NuclearWasteBlock(settings: Settings) : Block(settings), LCCExtendedBlock, LCCExtendedBlockContent {
 
     override fun onBlockAdded(state: BlockState, world: World, pos: BlockPos, oldState: BlockState, notify: Boolean) {
-        fall(world, pos, state, oldState)
+        fall(world, pos, state, oldState, initCascade = true)
     }
 
     override fun getStateForNeighborUpdate(state: BlockState, direction: Direction, newState: BlockState, world: WorldAccess, pos: BlockPos, posFrom: BlockPos): BlockState {
@@ -34,12 +35,12 @@ class NuclearWasteBlock(settings: Settings) : Block(settings), LCCExtendedBlock,
         return state
     }
 
-    override fun scheduledTick(state: BlockState, world: ServerWorld, pos: BlockPos, random: Random?) {
-        fall(world, pos, state, state_air)
+    override fun scheduledTick(state: BlockState, world: ServerWorld, pos: BlockPos, random: Random) {
+        fall(world, pos, state, state_air, random)
     }
 
-    fun fall(world: World, pos: BlockPos, state: BlockState, oldState: BlockState) {
-        val bp = BlockPos.Mutable().set(pos).move(0, 1, 0)
+    fun fall(world: World, pos: BlockPos, state: BlockState, oldState: BlockState, random: Random = world.random, initCascade: Boolean = false) {
+        val bp = BlockPos.Mutable().set(pos, 0, 1, 0)
         val fire = world.getBlockState(bp).isOf(LCCBlocks.nuclear_fire)
         bp.move(0, -1, 0)
         var flag = false
@@ -48,9 +49,27 @@ class NuclearWasteBlock(settings: Settings) : Block(settings), LCCExtendedBlock,
         }
         if (flag) {
             world.setBlockState(pos, oldState)
-            world.setBlockState(bp.move(0, 1, 0), state)
-            if (fire) world.setBlockState(bp.move(0, 1, 0), state_fire)
+            bp.move(0, 1, 0)
+
+            if (world.random.nextInt(12) == 0 || !cascadeFall(world, bp, state, fire, random)) {
+                world.setBlockState(bp, state)
+                if (fire) world.setBlockState(bp.move(0, 1, 0), state_fire)
+            }
+        } else if (initCascade && world.random.nextInt(2) == 0 && world.getBlockState(pos.down()).isOf(this) && cascadeFall(world, pos, state, fire, random)) {
+            world.setBlockState(pos, oldState)
         }
+    }
+
+    fun cascadeFall(world: World, pos: BlockPos, state: BlockState, fire: Boolean, random: Random = world.random): Boolean {
+        val bp = BlockPos.Mutable()
+        for (d in horizontalDirections.toList().apply { Collections.shuffle(this, random) }) {
+            if (world.getBlockState(bp.set(pos).move(d)).isAir && world.getBlockState(bp.move(0, -1, 0)).isAir) {
+                world.setBlockState(bp, state)
+                if (fire) world.setBlockState(bp.move(0, 1, 0), state_fire)
+                return true
+            }
+        }
+        return false
     }
 
     override fun lcc_content_nukeIgnore() = true
